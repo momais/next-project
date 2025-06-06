4caa4617078b9d6d3e9d9b17b2dd37dd30393525
momais1 <omaiskhan779@gmail.com>
2025-06-06 10:50:36 +0500

    Trigger fresh redeploy

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
     NEXT_PUBLIC_APP_URL: 'http://localhost:3000',
     // Add other environment variables here
   },
+  experimental: {
+    serverActions: true,
+  },
 };
 
 export default nextConfig;
